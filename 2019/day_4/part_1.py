439f0b6fe1dd8dc8f558ee3de5a186fdcee8c0d8
pingwindyktator <ja2222ja@gmail.com>
2019-12-04 12:02:50 +0100

    day 4

---
+++ 2019/day_4/part_1.py
@@ -0,0 +1,11 @@
+def may_be_password(number):
+    number = str(number)
+
+    return len(str(number)) == 6 and \
+           any((a == b for a, b in zip(number, number[1:]))) and \
+           all((a <= b for a, b in zip(number, number[1:])))
+
+
+if __name__ == '__main__':
+    input_range = (138241, 674034)
+    print(sum((may_be_password(number) for number in range(input_range[0], input_range[1] + 1))))
